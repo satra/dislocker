9c8ff66b4d85aa3d986e630d37fdc10f095b62bb
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2022-08-15 19:22:26 -0400

    fuse fix

--- cmake/FindFUSE.cmake
+++ cmake/FindFUSE.cmake
@@ -33,4 +33,4 @@ include ("FindPackageHandleStandardArgs")
 find_package_handle_standard_args ("FUSE" DEFAULT_MSG
     FUSE_INCLUDE_DIR FUSE_LIBRARIES)
 
-mark_as_advanced (FUSE_INCLUDE_DIRS FUSE_LIBRARIES FUSE_LIBRARY_DIRS)
+mark_as_advanced (FUSE_INCLUDE_DIRS FUSE_LIBRARIES)
